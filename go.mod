a71a373a8f1429a43e0b8d7728dc5ba12a15f3a2
Omprakashdewangan <prakash@albertapayments.com>
2025-09-05 21:52:25 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go-crypto-kafka
+
+go 1.24.3
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.49 // indirect
+)
---
+++ producer/producer.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type CryptoPrice struct {
+	Symbol string  `json:"symbol"`
+	Price  float64 `json:"price"`
+	Time   string  `json:"time"`
+}
+
+func main() {
+	writer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "crypto_prices",
+	})
+	defer writer.Close()
+
+	rand.Seed(time.Now().UnixNano())
+
+	for {
+		price := CryptoPrice{
+			Symbol: "BTCUSDT",
+			Price:  100000 + rand.Float64()*10000,
+			Time:   time.Now().Format(time.RFC3339),
+		}
+
+		data, _ := json.Marshal(price)
+		err := writer.WriteMessages(context.TODO(),
+			kafka.Message{
+				Key:   []byte(price.Symbol),
+				Value: data,
+			},
+		)
+		if err != nil {
+			log.Println("❌ Error writing to Kafka:", err)
+		} else {
+			log.Println("✅ Produced:", string(data))
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+}
